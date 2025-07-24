df671777a6700033f503199360c1df59ffea448b
Ayush Dash <120770632+ayushdash1302@users.noreply.github.com>
2025-07-24 14:37:04 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,19 @@
-output "pipeline_name" {
-  value = aws_codepipeline.pipeline.name # Update "my_pipeline" with your actual resource name.
+output "artifact_bucket" {
+  description = "S3 bucket used for storing artifacts"
+  value       = aws_s3_bucket.artifact_bucket.id
+}
+
+output "codepipeline_id" {
+  description = "ID of the CodePipeline"
+  value       = aws_codepipeline.devsecops_pipeline.id
+}
+
+output "codebuild_project_name" {
+  description = "Name of the CodeBuild project"
+  value       = aws_codebuild_project.devsecops_build.name
+}
+
+output "codedeploy_app_name" {
+  description = "Name of the CodeDeploy application"
+  value       = aws_codedeploy_app.devsecops_app.name
 }
