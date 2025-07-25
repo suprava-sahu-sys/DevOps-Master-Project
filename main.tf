209a32aa735b5e5659d21104556b59c93d51480f
Ayush Dash <120770632+ayushdash1302@users.noreply.github.com>
2025-07-25 11:24:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "eu-north-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -51,7 +51,7 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "ap-south-1b"
+  availability_zone       = "eu-north-1b"
   map_public_ip_on_launch = true
 
   tags = {
